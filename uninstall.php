8fc8d3a5e1622682cd78ae223376cbaf9d8a43bd
rianrietveld <rian@rrwd.nl>
2018-06-24 15:20:40 +0200

    phpcs

--- uninstall.php
+++ uninstall.php
@@ -1,17 +1,20 @@
 <?php
 /**
- * Uninstall for WP-Accessible
+ * Uninstall for WP-Accessible.
+ *
+ * @package wp-accessible
  */
 
-if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
-    exit();
+if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
 
-delete_option('widget_wpacc-latest-tweets');
-delete_option('wpacc_tdf_consumer_key');
-delete_option('wpacc_tdf_consumer_secret');
-delete_option('wpacc_tdf_access_token');
-delete_option('wpacc_tdf_access_token_secret');
-delete_option('wpacc_tdf_user_timeline');
-delete_option('wpacc_tdf_cache_expire');
+delete_option( 'widget_wpacc-latest-tweets' );
+delete_option( 'wpacc_tdf_consumer_key' );
+delete_option( 'wpacc_tdf_consumer_secret' );
+delete_option( 'wpacc_tdf_access_token' );
+delete_option( 'wpacc_tdf_access_token_secret' );
+delete_option( 'wpacc_tdf_user_timeline' );
+delete_option( 'wpacc_tdf_cache_expire' );
 
-?>
+
